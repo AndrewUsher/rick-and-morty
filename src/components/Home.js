83998446eee46d6de9431ec13f2eaff76ea65a70
Andrew Usher <andrewusher00@gmail.com>
2018-04-04 18:21:53 -0500

    :sparkles: Add home page

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h2>
+        Welcome to Mortypedia, the home of everything Rick and Morty! Feel free to look around at the characters,
+        locations, and episodes.
+      </h2>
+    </div>
+  )
+}
+
+export default Home
