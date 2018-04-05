78e1343f7c979267d336657e18ae7e01f4bb6b8f
Andrew Usher <andrewusher00@gmail.com>
2018-04-05 15:09:45 -0500

    :bug: Style loader

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,7 @@ import Loadmang from '../components/Loadmang'
 import Home from '../components/Home'
 import Navbar from '../components/Navbar'
 
-const Characters = Loadmang(() => import('../components/Characters').then(mod => mod.default))
+const Characters = Loadmang(() => import('../components/Characters'))
 const Locations = Loadmang(() => import('../components/Locations'))
 const Episodes = Loadmang(() => import('../components/Episodes'))
 
