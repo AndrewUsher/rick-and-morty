aa5313b8b9e0f0cebb660c29041ecb206a16bce6
Andrew Usher <andrewusher00@gmail.com>
2021-09-26 00:09:29 -0500

    refactor: remove react-loadable and switch to React.lazy

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component, Suspense } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { HashRouter, Route } from 'react-router-dom'
+import { PropagateLoader } from 'react-spinners'
 import Home from '../components/Home'
-import Loadmang from '../components/Loadmang'
 import Navbar from '../components/Navbar'
 
-const Characters = Loadmang(() => import('../components/Characters'))
-const Locations = Loadmang(() => import('../components/Locations'))
-const Episodes = Loadmang(() => import('../components/Episodes'))
-const SingleCharacter = Loadmang(() => import('../components/SingleCharacter'))
-const SingleLocation = Loadmang(() => import('../components/SingleLocation'))
-const SingleEpisode = Loadmang(() => import('../components/SingleEpisode'))
+const Characters = React.lazy(() => import('../components/Characters'))
+const Locations = React.lazy(() => import('../components/Locations'))
+const Episodes = React.lazy(() => import('../components/Episodes'))
+const SingleCharacter = React.lazy(() => import('../components/SingleCharacter'))
+const SingleLocation = React.lazy(() => import('../components/SingleLocation'))
+const SingleEpisode = React.lazy(() => import('../components/SingleEpisode'))
 
 const client = new QueryClient()
 
@@ -22,13 +22,15 @@ export default class App extends Component {
         <HashRouter>
           <div>
             <Navbar />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/characters" component={Characters} />
-            <Route exact path="/locations" component={Locations} />
-            <Route exact path="/episodes" component={Episodes} />
-            <Route exact path="/characters/:id" component={SingleCharacter} />
-            <Route exact path="/locations/:id" component={SingleLocation} />
-            <Route exact path="/episodes/:id" component={SingleEpisode} />
+            <Suspense fallback={<PropagateLoader size={25} />}>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/characters" component={Characters} />
+              <Route exact path="/locations" component={Locations} />
+              <Route exact path="/episodes" component={Episodes} />
+              <Route exact path="/characters/:id" component={SingleCharacter} />
+              <Route exact path="/locations/:id" component={SingleLocation} />
+              <Route exact path="/episodes/:id" component={SingleEpisode} />
+            </Suspense>
           </div>
         </HashRouter>
         <ReactQueryDevtools initialIsOpen={false} />
