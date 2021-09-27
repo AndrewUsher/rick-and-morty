d551f9dd66860d5990cddde02bef1d1fe5330a3d
Andrew Usher <andrewusher00@gmail.com>
2021-09-26 22:09:51 -0500

    refactor(app container): switch to function component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from 'react'
+import React, { Suspense } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { HashRouter, Route } from 'react-router-dom'
@@ -15,26 +15,24 @@ const SingleEpisode = React.lazy(() => import('../components/SingleEpisode'))
 
 const client = new QueryClient()
 
-export default class App extends Component {
-  render () {
-    return (
-      <QueryClientProvider client={client}>
-        <HashRouter>
-          <div>
-            <Navbar />
-            <Suspense fallback={<PropagateLoader size={25} />}>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/characters" component={Characters} />
-              <Route exact path="/locations" component={Locations} />
-              <Route exact path="/episodes" component={Episodes} />
-              <Route exact path="/characters/:id" component={SingleCharacter} />
-              <Route exact path="/locations/:id" component={SingleLocation} />
-              <Route exact path="/episodes/:id" component={SingleEpisode} />
-            </Suspense>
-          </div>
-        </HashRouter>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    )
-  }
+export function App () {
+  return (
+    <QueryClientProvider client={client}>
+      <HashRouter>
+        <div>
+          <Navbar />
+          <Suspense fallback={<PropagateLoader size={25} />}>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/characters" component={Characters} />
+            <Route exact path="/locations" component={Locations} />
+            <Route exact path="/episodes" component={Episodes} />
+            <Route exact path="/characters/:id" component={SingleCharacter} />
+            <Route exact path="/locations/:id" component={SingleLocation} />
+            <Route exact path="/episodes/:id" component={SingleEpisode} />
+          </Suspense>
+        </div>
+      </HashRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
